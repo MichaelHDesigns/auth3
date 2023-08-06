92ab8a10e5022d7b3a334318cd42a54d75b023f1
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-08-05 23:13:09 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/legacy/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<p> Currenlty Building Your New Service!!! </ p>
+<p> Currently Building Your New Service!!! </ p>
     </main>
   )
 }
